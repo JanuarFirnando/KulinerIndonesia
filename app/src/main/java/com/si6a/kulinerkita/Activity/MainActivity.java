9b2e6eaf6d7497d1918133fac3d6513362a8f44c
JanuarFirnando <januar.firnando@mhs.mdp.ac.id>
2023-04-05 09:26:16 +0700

    pembuatan package java

---
+++ app/src/main/java/com/si6a/kulinerkita/Activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.si6a.kulinerkita.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.si6a.kulinerkita.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/si6a/kulinerkita/Model/ModelResponse.java
@@ -0,0 +1,23 @@
+package com.si6a.kulinerkita.Model;
+
+
+
+import java.util.List;
+
+public class ModelResponse
+{
+    private String kode, pesan;
+    private List<ModelKuliner> data;
+
+    public String getKode() {
+        return kode;
+    }
+
+    public String getPesan() {
+        return pesan;
+    }
+
+    public List<ModelKuliner> getData() {
+        return data;
+    }
+}
