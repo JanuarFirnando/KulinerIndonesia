303e6b446480816ac01d5c7c44a480b5b5bae3a5
JanuarFirnando <januar.firnando@mhs.mdp.ac.id>
2023-04-17 09:22:36 +0700

    update API

---
+++ app/src/main/java/com/si6a/kulinerkita/API/RetroServer.java
@@ -0,0 +1,22 @@
+package com.si6a.kulinerkita.API;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetroServer {
+    private static final String alamatServer = "https://kulinerjanuar.000webhostapp.com/";
+    private static Retrofit retro;
+
+    public static Retrofit konekRetrofit()
+    {
+        if(retro == null)
+        {
+            retro = new Retrofit.Builder()
+                    .baseUrl(alamatServer)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retro;
+    }
+
+}
