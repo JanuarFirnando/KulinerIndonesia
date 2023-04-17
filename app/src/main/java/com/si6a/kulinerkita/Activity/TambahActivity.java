30acf5ed837b0a0ae92f52ae7f6c4f9a65400189
JanuarFirnando <januar.firnando@mhs.mdp.ac.id>
2023-04-17 08:38:16 +0700

    tugas desain

---
+++ app/src/main/java/com/si6a/kulinerkita/Activity/TambahActivity.java
@@ -0,0 +1,16 @@
+package com.si6a.kulinerkita.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.si6a.kulinerkita.R;
+
+public class TambahActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tambah);
+    }
+}
